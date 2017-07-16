af058f0d74fb1e837a7ab5074bec995369f64c24
satoTomoyuki <sato.tomoyuki1980@gmail.com>
2017-07-16 23:51:35 +0900

    サンプルコミット

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PushStartButton () {
+		SceneManager.LoadScene ("GameScene");	//ゲームシーンへ
+	}
+}
